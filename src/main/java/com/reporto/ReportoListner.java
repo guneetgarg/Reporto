802d614de889440c30ea4558ab953b6477e7783d
guneetgarg <guneetgarg@gmail.com>
2019-08-29 00:39:24 +0530

    stats result

--- src/main/java/com/reporto/ReportoListner.java
+++ src/main/java/com/reporto/ReportoListner.java
@@ -9,23 +9,30 @@ import java.util.Map;
 public class ReportoListner implements IReporter, ITestListener {
 
 
-    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
-                               String outputDirectory) {
-        System.out.println("*********************************");
-        System.out.println(xmlSuites.size());
-
-        System.out.println(suites.get(0).getHost());
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
 
 
         for (ISuite s : suites) {
             Map<String, ISuiteResult> suiteResults = s.getResults();
             for (ISuiteResult sr : suiteResults.values()) {
                 ITestContext testContext = sr.getTestContext();
-                System.out.println(testContext.getPassedTests().size());
-                System.out.println(testContext.getFailedTests().size());
-                System.out.println(testContext.getSkippedTests().size());
+                StatsResult.passed += testContext.getPassedTests().size();
+                StatsResult.failed += testContext.getFailedTests().size();
+                int retriedPerTest = 0;
+                int skippedPerTest = 0;
+                for (ITestResult result : testContext.getSkippedTests().getAllResults()) {
+                    if (result.wasRetried()) {
+                        retriedPerTest++;
+                    } else {
+                        skippedPerTest++;
+                    }
+                }
+                StatsResult.skipped += skippedPerTest;
+                StatsResult.retried += retriedPerTest;
+                StatsResult.ignored += testContext.getExcludedMethods().size();
             }
         }
+        System.out.printf("passed " + StatsResult.passed);
 
 
     }
---
+++ src/main/java/com/reporto/StatsResult.java
@@ -0,0 +1,9 @@
+package com.reporto;
+
+public class StatsResult {
+    public static int passed = 0;
+    public static int failed = 0;
+    public static int skipped = 0;
+    public static int ignored = 0;
+    public static int retried = 0;
+}
