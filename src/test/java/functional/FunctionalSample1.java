fcbd17af68202f20c5a7b94b96621b1b5cef5835
guneetgarg <guneetgarg@gmail.com>
2019-08-29 00:33:59 +0530

    testscript

---
+++ src/test/java/functional/FunctionalSample1.java
@@ -0,0 +1,33 @@
+package functional;
+
+
+import org.testng.annotations.Test;
+
+public class FunctionalSample1 {
+
+    @Test
+    public void TestA() {
+
+    }
+
+    @Test
+    public void TestB() {
+
+    }
+
+
+    @Test
+    public void TestC() {
+
+    }
+
+    @Test
+    public void TestD() {
+
+    }
+
+    @Test
+    public void TestE() {
+
+    }
+}
