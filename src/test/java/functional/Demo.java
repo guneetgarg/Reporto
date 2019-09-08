362c70aaab041e1dd4e2de5085135de33d53ffc0
guneetgarg <guneetgarg@gmail.com>
2019-09-09 01:25:54 +0530

    prop to json try

--- src/test/java/functional/Demo.java
+++ src/test/java/functional/Demo.java
@@ -2,32 +2,33 @@ package functional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.gson.Gson;
+import com.sun.xml.internal.fastinfoset.sax.Properties;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.json.JSONObject;
 import org.json.XML;
+import pl.jalokim.propertiestojson.util.PropertiesToJsonConverter;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.*;
 
 public class Demo {
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws FileNotFoundException {
 
-            InputStream inputStream = new FileInputStream(new File(
-                    System.getProperty("user.dir") + File.separator
-                            + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "configuration" + File.separator + "config.xml"));
+        String pathFile = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "config.properties";
 
-            String xml = IOUtils.toString(inputStream, "UTF-8");
-            JSONObject jObject = XML.toJSONObject(xml);
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-            Object json = mapper.readValue(jObject.toString(), Object.class);
-            String output = mapper.writeValueAsString(json);
-            System.out.println(output);
-        } catch (Exception e) {
-            e.getStackTrace();
-        }
+        System.out.println(pathFile);
+
+        File file = new File("/Users/guneetgarg/IdeaProjects/Reporto/src/main/resources/config.properties");
+        FileInputStream fileInput = new FileInputStream(file);
+
+
+        Properties properties = new Properties();
+        properties.load(Demo.class.getResourceAsStream("config.properties"));
 
 
     }
