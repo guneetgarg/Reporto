e3733167ab4793a98777203c9f648fde1efc5fcf
guneetgarg <guneetgarg@gmail.com>
2019-09-09 21:10:46 +0530

    util refactor

--- src/main/java/Util/Wrapper.java
+++ src/main/java/Util/Wrapper.java
@@ -1,39 +1,33 @@
 package Util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 import org.json.XML;
+import org.testng.Reporter;
 
 import java.io.*;
 import java.util.Properties;
 
 public class Wrapper {
 
-    public static JsonNode convertToJson(Object obj) throws IOException {
-
-        if (true) {
-            InputStream inputStream = new FileInputStream(new File(
-                    System.getProperty("user.dir") + File.separator
-                            + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "configuration" + File.separator + "config.xml"));
-
-            String xml = IOUtils.toString(inputStream, "UTF-8");
-            JSONObject jObject = XML.toJSONObject(xml);
+    public static JsonNode convertToJson(File file) throws IOException {
+        String output = null;
+        InputStream inputStream = new FileInputStream(new File(
+                System.getProperty("user.dir") + File.separator
+                        + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "configuration" + File.separator + "config.xml"));
+        if (getExtension(file).equalsIgnoreCase("xml")) {
+            JSONObject jObject = XML.toJSONObject(IOUtils.toString(inputStream, "UTF-8"));
             ObjectMapper mapper = new ObjectMapper();
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
             Object json = mapper.readValue(jObject.toString(), Object.class);
-            String output = mapper.writeValueAsString(json);
-        } else if (true) {
-            InputStream inputStream = new FileInputStream(new File(
-                    System.getProperty("user.dir") + File.separator
-                            + "src" + File.separator + "main" + File.separator + "resources" + File.separator
-                            + "configuration" + File.separator + "config.properties"));
-
+            output = mapper.writeValueAsString(json);
+        } else if (getExtension(file).equalsIgnoreCase("properties")) {
 
             Properties prop = new Properties();
 
@@ -41,26 +35,53 @@ public class Wrapper {
                 System.out.println("Sorry, unable to find config.properties");
                 return null;
             }
-
-            //load a properties file from class path, inside static method
             prop.load(inputStream);
-
-            //get the property value and print it out
-            System.out.println(prop.getProperty("db.url"));
-            System.out.println(prop.getProperty("object.man.name"));
-
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String strJson = gson.toJson(prop);
-            System.out.println(strJson);
+            output = gson.toJson(prop);
+            System.out.println(output);
+        } else if (getExtension(file).equalsIgnoreCase("json")) {
+
         }
 
-
+        System.out.println(output);
         return null;
     }
 
 
-    public void checkFileType() {
+    public static void checkFileType() {
+        String path = System.getProperty("user.dir") + File.separator
+                + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "configuration";
+        File folder = new File(path);
+        if (folder.exists()) {
+            path += File.separator + getEnvValue();
+            folder = new File(path);
+            System.out.println(folder.exists());
+        } else {
+
+        }
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+                System.out.println(FilenameUtils.getExtension(listOfFiles[i].getName()));
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
 
     }
 
+    public static String getExtension(File fileName) {
+        return FilenameUtils.getExtension(fileName.getName());
+    }
+
+    public static String getEnvValue() {
+        if (!(Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter("environment") == null)) {
+            return Reporter.getCurrentTestResult().getTestContext().getCurrentXmlTest().getParameter("environment");
+        } else if (!(System.getProperty("env") == null)) {
+            return System.getProperty("env");
+        }
+        return null;
+    }
 }
